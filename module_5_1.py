163ec92bf942292f37087e7c747ad2da53b87d4e
Smyyug <100402danil@gmail.com>
2024-11-24 21:32:54 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -6,7 +6,7 @@ class House:
         if new_floor < 1 or new_floor > self.number_of_floors:
             print("Такого этажа не существует")
         else:
-            for i in range(1, new_floor):
+            for i in range(1, new_floor + 1):
                 print(i)
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
